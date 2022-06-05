bc7f175f98bc215a31edaa3d507cd989909b417c
Ikram Mustofa Kamal <ikram.mustofa07@gmail.com>
2022-06-05 23:25:23 +0700

    feat: menambahkan fungsi sorter. Ascending dan Descending
    fungsi sorter membutuhkan 2 parameter yaitu:
    1. array of strings yang akan di sort.
    2. function logic untuk sorternya (asc/desc).

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrName, cbLogicSorter) => {
+  // create container array
+  let arrStringOutput = [];
+
+  // loop to fill the container array
+  for(counter = 0; counter < arrName.length; counter++) {
+    // fill the container array with callback logic sorter.
+    arrStringOutput.push(`${counter+1}. ${cbLogicSorter(arrName)[counter]}`)
+  }
+
+  // return a sorter array of string
+  return arrStringOutput;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (ascName) => ascName.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (descName) => descName.sort().reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
